73dd6f7b4d5c991d3d9bfc04b3cbd85df2a4646a
NitinNitinRaj <bnitinraj@gmail.com>
2024-01-23 11:22:56 +0530

    color Palette generator

--- color_project/src/App.js
+++ color_project/src/App.js
@@ -1,7 +1,9 @@
 import Palette from "./components/Palette";
+import { generatePalette } from "./utils/colorHelper";
 import seedColors from "./utils/seedColors";
 
 export default function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
       <Palette {...seedColors[2]} />
--- color_project/src/components/Palette.js
+++ color_project/src/components/Palette.js
@@ -1,7 +1,9 @@
 import ColorBox from "./ColorBox";
 import "./Palette.css";
 export default function Palette({ colors }) {
-  const colorBoxes = colors.map((color) => <ColorBox background={color} />);
+  const colorBoxes = colors.map((color) => (
+    <ColorBox key={color.name} background={color} />
+  ));
 
   return (
     <div className="Palette">
