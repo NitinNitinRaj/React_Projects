43a95d633cc798a4567d618356d7b4224ca656c2
NitinNitinRaj <bnitinraj@gmail.com>
2024-02-06 10:53:41 +0530

    color slider

--- color_project/src/components/Palette.js
+++ color_project/src/components/Palette.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import ColorBox from "./ColorBox";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
 import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
 
 export default function Palette({ palette: { colors } }) {
   const [level, setLevel] = useState(500);
@@ -14,13 +14,27 @@ export default function Palette({ palette: { colors } }) {
   return (
     <div className="Palette">
       {/* NavBar here */}
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        step={100}
-        onChange={(value) => setLevel(value)}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          styles={{
+            track: { backgroundColor: "transparent" },
+            rail: { height: "8px" },
+            handle: {
+              backgroundColor: "green",
+              outline: "none",
+              border: "2px solid green",
+              boxShadow: "none",
+              width: "15px",
+              height: "15px",
+            },
+          }}
+          onChange={(value) => setLevel(value)}
+        />
+      </div>
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer here */}
     </div>
