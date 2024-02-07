6110732157066467e0130aa80441529a514a07c8
NitinNitinRaj <bnitinraj@gmail.com>
2024-02-07 11:00:41 +0530

    start styling PaletteList

--- color_project/src/components/PaletteList.js
+++ color_project/src/components/PaletteList.js
@@ -3,13 +3,19 @@ import MiniPalette from "./MiniPalette";
 
 export default function PaletteList({ palettes }) {
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map((palette) => (
-        <p>
-          <MiniPalette {...palette} />
-        </p>
-      ))}
+    <div className="bg-blue-500 h-full flex items-start justify-center">
+      <div className="w-1/2 flex flex-col items-start flex-wrap">
+        <nav className="flex w-full justify-between text-white">
+          <h1>React Colors</h1>
+        </nav>
+        <div className="box-border w-full grid grid-cols-3 gap-[5%]">
+          {palettes.map((palette) => (
+            <p>
+              <MiniPalette {...palette} />
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
