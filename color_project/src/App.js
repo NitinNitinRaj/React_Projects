712fbd8f294a97a75f7de462ff1068913f97387c
NitinNitinRaj <bnitinraj@gmail.com>
2024-02-09 10:16:34 +0530

    add drawer to new Palette form

--- color_project/src/App.js
+++ color_project/src/App.js
@@ -3,12 +3,15 @@ import PaletteList from "./components/PaletteList";
 import PaletteWrapper from "./components/PaletteWrapper";
 import SingleColorPaletteWrapper from "./components/SingleColorPaletteWrapper";
 import seedColors from "./utils/seedColors";
+import NewPaletteForm from "./components/NewPaletteFrom";
 
 export default function App() {
   return (
     <Routes>
       <Route exact path="/" element={<PaletteList palettes={seedColors} />} />
+      <Route exact path="/palette/new" element={<NewPaletteForm />} />
       <Route exact path="/palette/:id" element={<PaletteWrapper />} />
+
       <Route
         exact
         path="/palette/:paletteId/:colorId"
--- color_project/src/components/PaletteList.js
+++ color_project/src/components/PaletteList.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
 
 export default function PaletteList({ palettes }) {
@@ -11,8 +11,9 @@ export default function PaletteList({ palettes }) {
   return (
     <div className="bg-blue-500 h-screen flex items-start justify-center">
       <div className="w-1/2 flex flex-col items-start flex-wrap">
-        <nav className="flex w-full justify-between text-white">
-          <h1>React Colors</h1>
+        <nav className="flex w-full justify-between text-white items-center py-4">
+          <h1 className="text-lg">React Colors</h1>
+          <Link to="/palette/new">Create Palette </Link>
         </nav>
         <div className="box-border w-full grid grid-cols-3 gap-[5%]">
           {palettes.map((palette) => (
