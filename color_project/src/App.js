d3069bc6f1a50347c933d45e5f91f302f395652d
NitinNitinRaj <bnitinraj@gmail.com>
2024-02-13 14:27:02 +0530

    save palettes to local storage

--- color_project/src/App.js
+++ color_project/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NewPaletteForm from "./components/NewPaletteFrom";
 import PaletteList from "./components/PaletteList";
@@ -7,10 +7,21 @@ import SingleColorPaletteWrapper from "./components/SingleColorPaletteWrapper";
 import seedColors from "./utils/seedColors";
 
 export default function App() {
-  const [palettes, setPalettes] = useState(seedColors);
+  const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+  const [palettes, setPalettes] = useState(savedPalettes || seedColors);
+
   const savePalette = (newPalette) => {
     setPalettes([...palettes, newPalette]);
   };
+
+  useEffect(() => {
+    syncLocalStorage();
+  }, [palettes]);
+
+  const syncLocalStorage = () => {
+    window.localStorage.setItem("palettes", JSON.stringify(palettes));
+  };
+
   return (
     <Routes>
       <Route exact path="/" element={<PaletteList palettes={palettes} />} />
